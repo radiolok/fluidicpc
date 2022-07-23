c5f724f305b1e6623748738b58419f5b554a49aa
Artem Kashkanov <radiolok@yandex.ru>
2022-07-23 22:35:53 +0300

    Add initial fibonachi code

---
+++ fibonachi/fibonachi_tb.v
@@ -0,0 +1,43 @@
+module fibonachi_tb();
+
+
+parameter WIDTH=4;
+parameter TEST_NUM=10;
+wire [WIDTH-1:0] out;
+reg clk;
+reg rst;
+
+reg [$clog2(TEST_NUM)-1:0] test;
+
+fibonachi #(
+.WIDTH(WIDTH)
+) Fibonachi(
+.clk(clk),
+.rst(rst),
+.out(out)
+);
+
+initial begin 
+  $dumpfile("fibonachi.vcd");
+  $dumpvars(0, fibonachi_tb);
+  clk = 1;
+  rst = 1;
+  test = 0;
+  #1 rst = 0;
+  forever #1 clk = ~clk;
+
+end
+
+
+always @(posedge clk) begin
+  test <= test + 1;
+  if (test >= TEST_NUM) begin
+    $finish();
+  end
+  $display("%d: %d", test, out);
+
+end
+
+endmodule
+
+
---
+++ fibonachi/run_synt.tcl
@@ -0,0 +1,49 @@
+if { $argc != 2 } {
+  puts "call script <file_with_v_list> <top_level_module>".
+} else {
+}
+yosys -import
+
+yosys read_verilog [lindex $argv 0] 
+# read design
+hierarchy -check
+yosys synth -top [lindex $argv 1]
+#
+# # high-level synthesis
+yosys proc 
+yosys opt 
+yosys memory
+yosys opt
+yosys fsm
+yosys opt
+#
+# # low-level synthesis
+yosys techmap
+yosys opt
+#
+
+set yosys_path "$::env(HOME)/yosys"
+set cell_lib "fluidic.lib"
+# # map to target architecture
+yosys read_liberty -lib $cell_lib 
+yosys dfflibmap -liberty $cell_lib 
+yosys abc -liberty $cell_lib 
+#
+# # split larger signals
+yosys splitnets -ports
+yosys opt
+#
+# # cleanup
+yosys clean
+#
+# # write synthesized design
+yosys write_verilog [lindex $argv 1]_synth.v
+#
+# # write intermediate language
+yosys write_ilang [lindex $argv 1]_ilang.txt
+#
+# # show
+yosys show -format dot -lib [lindex $argv 1]_synth.v -prefix [lindex $argv 1]
+yosys stat
+yosys stat -liberty $cell_lib 
+exec dot -Tpng [lindex $argv 1].dot > [lindex $argv 1].png
