a6853129fa14555499aa1b951aedfbdb7422a407
Artem Kashkanov <radiolok@yandex.ru>
2022-07-23 23:57:29 +0300

    Just adder

--- fibonachi/run_synt.sh
+++ fibonachi/run_synt.sh
@@ -2,8 +2,6 @@
 
 set -Eeuo pipefail
 
-iverilog -o fibonachi -s fibonachi_tb fibonachi.v fibonachi_tb.v -g2012
-./fibonachi
 
 echo 'tcl run_synt.tcl fibonachi.v fibonachi'  > tcl.txt
 yosys < tcl.txt
