3456c39ce7f101bedd5998fef2033e310ff0b4e9
Artem Kashkanov <radiolok@yandex.ru>
2022-08-10 21:52:51 +0300

    Rewriten to use FA

--- adder/adder.v
+++ adder/adder.v
@@ -1,3 +1,16 @@
+
+module FA(
+input wire ci,
+input wire A,
+input wire B,
+output wire out,
+output wire co
+);
+
+assign {co,out} = A + B + ci;
+
+endmodule
+
 module adder #(
 parameter WIDTH=4
 )(
@@ -8,7 +21,26 @@ output wire [WIDTH-1:0] out,
 output wire co
 );
 
-assign {co,out} = A + B + ci;
+genvar i;
+generate
+
+  for (i=0; i < WIDTH; i=i+1) begin: Add
+    wire c_in;
+    wire c_out;
+    if (i==0) begin
+      assign c_in = ci;
+    end
+    else begin
+      assign c_in = Add[i-1].c_out;
+    end
+    FA fa(.A(A[i]),
+          .B(B[i]),
+          .out(out[i]),
+          .ci(c_in),
+          .co(c_out));
+  end
+
+endgenerate
 
 endmodule
 
--- adder/run_synt.sh
+++ adder/run_synt.sh
@@ -12,3 +12,9 @@ iverilog -o adder -P adder_tb.WIDTH=${WIDTH} -s adder_tb adder_tb.v adder.v -g20
 echo 'tcl run_synt.tcl adder.v adder '${WIDTH}  > tcl.txt
 yosys < tcl.txt
 
+gvpr -f split adder.dot
+rm -f *.png
+
+for file in $(ls *.dot); do 
+    echo $file; dot -Tpng $file -O
+done
